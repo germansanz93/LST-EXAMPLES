8b77a741d17bf8994c484898868940fa23cbd78f
German Sanz <64764636+germansanz93@users.noreply.github.com>
2023-08-27 11:38:02 -0300

    Se modifica el getFiles para que obtenga los archivos

--- curso_git_github/getFiles.cmd
+++ curso_git_github/getFiles.cmd
@@ -1,3 +1,3 @@
-curl -OL https://raw.githubusercontent.com/germansanz93/LST-EXAMPLES/main/curso_git_github/style.css
-curl -OL https://raw.githubusercontent.com/germansanz93/LST-EXAMPLES/main/curso_git_github/desktop.jpg
-curl -OL https://raw.githubusercontent.com/germansanz93/LST-EXAMPLES/main/curso_git_github/index.html
+curl -OL https://raw.githubusercontent.com/germansanz93/LST-EXAMPLES/main/curso_git_github/git/style.css
+curl -OL https://raw.githubusercontent.com/germansanz93/LST-EXAMPLES/main/curso_git_github/git/desktop.jpg
+curl -OL https://raw.githubusercontent.com/germansanz93/LST-EXAMPLES/main/curso_git_github/git/index.html
